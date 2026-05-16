03b116209a0ae6b7daa5fb226aafe04a7bf9c06f
SAIKIRAN <153582814+PRONOOB157@users.noreply.github.com>
2026-05-17 02:53:05 +0530

    Fix version history diff showing words in reverse order

--- frontend/src/utils/diffText.js
+++ frontend/src/utils/diffText.js
@@ -25,37 +25,42 @@ const buildLcsTable = (a, b) => {
   return dp;
 };
 
+const coalesceSegments = (parts) => {
+  const merged = [];
+
+  for (const part of parts) {
+    const last = merged[merged.length - 1];
+    if (last && last.type === part.type) {
+      last.value += part.value;
+    } else {
+      merged.push({ type: part.type, value: part.value });
+    }
+  }
+
+  return merged;
+};
+
 const diffTokens = (oldTokens, newTokens) => {
   const dp = buildLcsTable(oldTokens, newTokens);
   const segments = [];
   let i = oldTokens.length;
   let j = newTokens.length;
 
-  const push = (type, value) => {
-    if (!value) return;
-    const last = segments[segments.length - 1];
-    if (last && last.type === type) {
-      last.value += value;
-    } else {
-      segments.push({ type, value });
-    }
-  };
-
   while (i > 0 || j > 0) {
     if (i > 0 && j > 0 && oldTokens[i - 1] === newTokens[j - 1]) {
-      push('equal', oldTokens[i - 1]);
+      segments.push({ type: 'equal', value: oldTokens[i - 1] });
       i -= 1;
       j -= 1;
     } else if (j > 0 && (i === 0 || dp[i][j - 1] >= dp[i - 1][j])) {
-      push('add', newTokens[j - 1]);
+      segments.push({ type: 'add', value: newTokens[j - 1] });
       j -= 1;
     } else {
-      push('remove', oldTokens[i - 1]);
+      segments.push({ type: 'remove', value: oldTokens[i - 1] });
       i -= 1;
     }
   }
 
-  return segments.reverse();
+  return coalesceSegments(segments.reverse());
 };
 
 export const diffText = (oldText = '', newText = '') => {
